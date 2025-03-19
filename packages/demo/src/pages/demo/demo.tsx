c2c384fd885a9489584ee3efb70f702167a80625
LUSIA.L <lusia.liang@sjfood.com>
2025-03-19 10:59:24 +0800

    feat(demo): Update demo by using flow component

--- packages/demo/src/pages/demo/demo.tsx
+++ packages/demo/src/pages/demo/demo.tsx
@@ -1,9 +1,26 @@
-import { HelloWorld } from "one-pass-flow";
-import { FC } from "react";
-export const Demo: FC = () => {
+import { useMount } from "ahooks";
+import {
+  ONE_PASS_FLOW_DEFAULT_NODE_TYPES,
+  OnePassFlow,
+  OnePassFlowRefType,
+} from "one-pass-flow";
+import { useRef } from "react";
+
+import data from "./data.json";
+
+export const Demo = () => {
+  const flowRef = useRef<OnePassFlowRefType>(null);
+
+  useMount(() => {
+    flowRef.current?.handleSetData(data);
+  });
+
   return (
-    <div className="text-center">
-      <HelloWorld />
+    <div className="text-center h-screen">
+      <OnePassFlow
+        flowRef={flowRef}
+        nodeTypes={ONE_PASS_FLOW_DEFAULT_NODE_TYPES}
+      />
     </div>
   );
 };
