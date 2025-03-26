4f38ff2e60cddb029bbcc3f09474dfb01cd38385
LUSIA.L <lusia.liang@sjfood.com>
2025-03-26 10:37:59 +0800

    feat(demo): Update demo with `edgeTypes` configuration example

--- packages/demo/src/pages/demo/demo.tsx
+++ packages/demo/src/pages/demo/demo.tsx
@@ -1,5 +1,8 @@
 import { useMount } from "ahooks";
 import {
+  AddEdge,
+  EdgeComponentType,
+  EdgeTypes,
   ONE_PASS_FLOW_DEFAULT_NODE_TYPES,
   OnePassFlow,
   OnePassFlowRefType,
@@ -7,6 +10,55 @@ import {
 import { useRef } from "react";
 
 import data from "./data.json";
+
+const EdgesType: EdgeTypes = {
+  AddEdge: (rest: EdgeComponentType) => (
+    <AddEdge
+      edge={rest}
+      // renderForm={(_, __, onClose) => (
+      //   <div
+      //     className=" text-purple-400 fixed "
+      //     onClick={() => {
+      //       console.log("onClick");
+      //       onClose();
+      //     }}
+      //   >
+      //     {"AAAA"}
+      //   </div>
+      // )}
+    />
+  ),
+  ConditionEdge: (rest: EdgeComponentType) => (
+    <AddEdge
+      edge={rest}
+      isCondition
+      // renderForm={(type, data, onClose) => (
+      //   <div
+      //     className=" text-purple-400 fixed "
+      //     onClick={() => {
+      //       onClose();
+      //     }}
+      //   >
+      //     {"AAAA"}
+      //   </div>
+      // )}
+    />
+  ),
+  EndEdge: (rest: EdgeComponentType) => (
+    <AddEdge
+      edge={rest}
+      isEnd
+      addButtonPosition={(edge) => ({
+        x: edge.targetX,
+        y: (edge.targetY + edge.sourceY) / 2,
+      })}
+      renderEdgeLabel={(edge, addButton) =>
+        ((edge.data?.parentIds as Array<any>)?.length ?? 0) > 1 &&
+        addButton(edge.targetX, edge.targetY - 20, ["ConditionNode"])
+      }
+    />
+  ),
+};
 
 export const Demo = () => {
   const flowRef = useRef<OnePassFlowRefType>(null);
@@ -20,6 +72,7 @@ export const Demo = () => {
       <OnePassFlow
         flowRef={flowRef}
         nodeTypes={ONE_PASS_FLOW_DEFAULT_NODE_TYPES}
+        edgeTypes={EdgesType}
       />
     </div>
   );
